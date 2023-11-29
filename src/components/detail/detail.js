d4b39035fb397f51730f54ed6f34aaf5beabddc4
yeonsoo1998 <k115502@Naver.com>
2023-11-29 14:50:05 +0900

    detail페이지 1차 수정(버튼추가 및 css)

--- src/components/detail/detail.js
+++ src/components/detail/detail.js
@@ -1,19 +1,56 @@
 import Toolbar from "../toolbar/toolbar";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./detail.css";
 
 function Detail() {
   const location = useLocation();
   const bookData = location.state?.bookData || {};
   console.log("bookData", bookData);
+  const navigate = useNavigate();
+  const isLoggedIn = false; // 로그인 상태 여부
+  const [isAddedToWishlist, setIsAddedToWishlist] = useState(false);
+
+  const handleAddToWishlist = () => {
+    //백엔드 와 통신내용 추가 필요
+    setIsAddedToWishlist(!isAddedToWishlist);
+  };
+
+  const handleBuy = () => {
+    if (!isLoggedIn) {
+      navigate("/signup");
+    }
+  };
+
+  const handleAuction = () => {
+    if (!isLoggedIn) {
+      navigate("/signup");
+    }
+  };
 
   return (
     <div>
       <Toolbar />
-      <div>
+
+      <div className="book_container">
         <img src={bookData.img} alt={bookData.title} />
         <h2>{bookData.title}</h2>
         <p>작가: {bookData.writer}</p>
-        <p>가격: {bookData.price}</p>
+        <p>즉시 구매가: {bookData.price}</p>
+      </div>
+      <div className="button_container">
+        <button className="button_buy" onClick={handleBuy}>
+          즉시구매
+        </button>
+        <button className="button_auction" onClick={handleAuction}>
+          경매입찰
+        </button>
+      </div>
+      <div className="wishlist">
+        {/* 관심 상품으로 찜하기 버튼 */}
+        <button className="button_wishlist" onClick={handleAddToWishlist}>
+          {isAddedToWishlist ? "찜 해제" : "관심 상품으로 찜하기"}
+        </button>
       </div>
     </div>
   );
